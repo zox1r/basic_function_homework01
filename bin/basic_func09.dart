e42ae612cfdeb4a69bb12abc9626abd6ee5b747b
zox1r <qarshiyevzoxir@gmail.com>
2023-07-22 11:26:22 +0200

    bin/basic_func09.dart

--- bin/basic_func09.dart
+++ bin/basic_func09.dart
@@ -3,7 +3,10 @@
 // Find the remainder of the division of number by 2 and assign it to ‘answer’.
 // return the answer.
 int func(int number) {
-  return 0;
+  int x = number % 2;
+  return x;
 }
 
-void main() {}
+void main() {
+  print(func(7));
+}
