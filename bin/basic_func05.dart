39d913887c97586faca25f902cf998a212f06293
zox1r <qarshiyevzoxir@gmail.com>
2023-07-22 11:10:30 +0200

    bin/basic_func05.dart

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -2,7 +2,11 @@
 // Create a fucntion argument ‘number’ two digits numbers will be given.
 // return the sum of the number’s digits.
 int func(int number) {
-  return 0;
+  int x = number % 10;
+  int y = number ~/ 10;
+  return x + y;
 }
 
-void main() {}
+void main() {
+  print(func(55));
+}
